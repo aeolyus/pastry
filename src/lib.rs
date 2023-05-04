87750c74e4e4d951e3f5fb4d34e534ecd3c4c835
Richard Huang <richardhuang.huang@gmail.com>
2023-05-03 23:21:39 -0700

    [lib] Refactor into skeleton for extensible contexts

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,62 @@
 use anyhow::Result;
-use reqwest;
+use clap::{Parser, ValueEnum};
 use reqwest::blocking::{multipart, Client};
 use std::io::{self, Read};
 
-// TODO: Make adjustable
-const ENDPOINT: &str = "https://0x0.st";
-/// Package the input string into a multipart form
-fn upload(input: String) -> Result<String, reqwest::Error> {
-    let mut data = multipart::Form::new();
-    let part = multipart::Part::text(input).file_name("");
-    data = data.part("file", part);
+trait Pastebin<'a> {
+    fn upload(self, input: String) -> Result<String>;
+}
 
-    // Send a POST request to the endpoint
-    let client = Client::new();
-    let response = client.post(ENDPOINT).multipart(data).send()?;
+#[derive(Clone, Debug)]
+struct TheNullPointer<'a> {
+    // The URL of the endpoint
+    endpoint: &'a str,
+}
 
-    return response.text();
+impl<'a> Pastebin<'a> for TheNullPointer<'a> {
+    fn upload(self, input: String) -> Result<String> {
+        // Package the input string into a multipart form
+        let mut data = multipart::Form::new();
+        let part = multipart::Part::text(input).file_name("");
+        data = data.part("file", part);
+        // Send a POST request to the endpoint
+        let client = Client::new();
+        let response = client.post(self.endpoint).multipart(data).send()?;
+
+        response.text().map_err(anyhow::Error::from)
+    }
+}
+
+/// Represents the endpoint API type so we can interact properly
+#[derive(Clone, ValueEnum, Debug)]
+enum EndpointApi {
+    TheNullPointer,
+}
+
+#[derive(Parser)]
+#[command(author, version, about)]
+pub struct Args {
+    /// The API we want to use for the endpoint
+    #[arg(short, long)]
+    api: EndpointApi,
 }
 
 /// Read the input from stdin
 fn read_input() -> Result<String> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
-    return Ok(input);
+    Ok(input)
 }
 
 /// Reads from stdin and uploads to a pastebin backend
 pub fn pastry() -> Result<String> {
     let result = read_input()?;
-    let url = upload(result)?;
-    return Ok(url);
+    let args = Args::parse();
+    let url = match args.api {
+        EndpointApi::TheNullPointer => TheNullPointer {
+            endpoint: "https://0x0.st",
+        }
+        .upload(result),
+    }?;
+    Ok(url)
 }
